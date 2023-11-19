04bbf6b5dff98ec9d9972a0a3363d7cebcc6d6ab
Celestin25 <celestinhakorimana25@gmail.com>
2023-11-19 20:58:03 +0200

    changed

--- contact_management/android/app/src/main/kotlin/com/jerondev/firebase_cli/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.jerondev.firebase_cli
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- contact_management/lib/utils/my_firebase.dart
+++
@@ -1,6 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-abstract class MyFirebase {
-  static final _db = FirebaseFirestore.instance;
-  static final contactsCollection = _db.collection('contacts');
-}
